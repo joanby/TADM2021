473bc47df9c1a7ec19b4201bdeb2d8e1d2836284
Juan Gabriel Gomila <juangabriel@frogames.es>
2020-10-21 18:00:48 +0200

    Primeros ficheros listos

---
+++ Prueba 1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 21 16:24:49 2020
+
+@author: juangabriel
+"""
+
+print("Hola")
+
+#x = input("Escribe aquí tu nombre: ")
+
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3], [1,5,20])
